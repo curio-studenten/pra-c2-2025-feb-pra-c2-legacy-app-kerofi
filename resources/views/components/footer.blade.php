a7a282cb9bf71df1272b8d048d3e49b723a8d3af
kelvino04 <kelvinsophie04@gmail.com>
2025-03-13 09:37:51 +0100

    ticket 6
    footer uitgebreid

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,28 +1,61 @@
 
 <footer>
-	© {{ __('misc.copyright') }}
-</footer>
+    <a href="#" class="fa fa-facebook"></a>
+    <div class="footer-content">
+
+        <div class="footer-about">
+            <h3>Over ons</h3>
+            <p>Wij zijn een innovatief bedrijf dat zich richt op het leveren van de beste producten en diensten voor onze klanten. Onze missie is om de wereld te verbeteren door middel van technologie en duurzaamheid.</p>
+        </div>
 
 
-<!-- analytics code -->              
-<script type="text/javascript">
+        <div class="footer-contact">
+            <h3>Contactgegevens</h3>
+            <ul>
+                <li><strong>Adres:</strong><a href="https://www.google.com/maps/place/Slotlaan+40,+4902+AE+Oosterhout/@51.6346177,4.8565121,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6992542558d13:0x1fb9692f1d8cc0e5!8m2!3d51.6346177!4d4.8587008">
+                    <button class="button" onclick="https://www.google.com/maps/place/Slotlaan+40,+4902+AE+Oosterhout/@51.6346177,4.8565121,17z/data=!3m1!4b1!4m5!3m4!1s0x47c6992542558d13:0x1fb9692f1d8cc0e5!8m2!3d51.6346177!4d4.8587008">  Slotlaan 40 4902 AE Oosterhout NB</a
+                        ></button></li>
+                            <li><strong>telefoonnummer:</strong><span itemprop="telephone"><a href="tel:+31 615657113"><button class="button" onclick="tel:+31 ;615657113">+0162 123456789</a></span></button></li>
+                            <li><strong>e-mail:</strong><br /><a href="https://mail.google.com/mail/?view=cm&fs=1&to=kelvinsophie04@gmail.com"><button class="button" onclick="https://mail.google.com/mail/?view=cm&fs=1&to=info@malisimo.nl">example@gmail.nl</a></button></li>
+                        </ul>
+                    </div>
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-30506707-1']);
-  _gaq.push(['_trackPageview']);
+                    <div class="footer-social">
+                        <h3>Volg ons</h3>
+                        <ul>
+                            <li><a href="https://www.facebook.com/" target="_blank">Facebook</a></li>
+                            <li><a href="https://www.twitter.com/" target="_blank">Twitter</a></li>
+                            <li><a href="https://www.instagram.com/" target="_blank">Instagram</a></li>
+                            <li><a href="https://www.linkedin.com/" target="_blank">LinkedIn</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-<!-- Einde analytics code -->
+                <div class="footer-bottom">
+                    <p>© {{ __('misc.copyright') }}</p>
+                </div>
+            </footer>
 
-<script language="Javascript" type="text/javascript"> 
- 
- if (top.location!= self.location) { 
-  top.location = self.location.href
- } 
- 
-</script>
+
+            <!-- analytics code -->
+            <script type="text/javascript">
+
+                var _gaq = _gaq || [];
+                _gaq.push(['_setAccount', 'UA-30506707-1']);
+                _gaq.push(['_trackPageview']);
+
+                (function() {
+                    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+                })();
+            </script>
+            <!-- Einde analytics code -->
+
+            <script language="Javascript" type="text/javascript">
+
+                if (top.location!= self.location) {
+                    top.location = self.location.href
+                }
+
+            </script>
